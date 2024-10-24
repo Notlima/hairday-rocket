87f54073908c083b44b2e8222e56d5ead60503de
Amilton Castro <amiltonscjunior@gmail.com>
2024-10-24 20:42:10 +0100

    create function to show schedules on the day selected

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,12 +1,20 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js"
+import { schedulesShow } from "../schedules/show.js"
 import { hoursLoad } from "../form/hours-load.js"
 
 //Seleciona o input de data.
 const selectDate = document.getElementById("date")
 
-export function scheduleDay(){
+export async function scheduleDay(){
   //Obtém a data do input
   const date = selectDate.value
 
+  //Busca na API os agendamentos
+  const dailySchedules = await scheduleFetchByDay({date})
+
+  //Exibe os agendamentos.
+  schedulesShow({ dailySchedules })
+
   //Renderza as horas disponiveis.
-  hoursLoad({ date })
+  hoursLoad({ date, dailySchedules })
 }
